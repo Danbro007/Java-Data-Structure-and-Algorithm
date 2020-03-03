12eb57ad4dc9346dfcaed55301ef643a8dc1ee95
danbro <710170342@qq.com>
2020-03-03 20:17:37 +0800

    添加插入排序算法

---
+++ 代码/数据结构/src/Algorithm/InsertSort.java
@@ -0,0 +1,45 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @Classname InsertSort
+ * @Description TODO 插入排序
+ * @Date 2020/3/3 20:00
+ * @Author Danrbo
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] array = {3, 9, -10, 10, 20};
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     *
+     * @param array 需要排序的数组
+     */
+    public static void insertSort(int[] array) {
+        //从数组第二个元素开始遍历
+        for (int i = 1; i < array.length; i++) {
+            //要插入的元素
+            int cur = array[i];
+            //要插入元素的前一个元素的索引
+            int index = i - 1;
+            //要插入元素的往有序队列遍历比较
+            // 遍历条件是有序队列没遍历完
+            // 并且要插入的元素比有序队列里比较的元素小
+            while (index >= 0 && cur < array[index]) {
+                array[index + 1] = array[index];
+                //index -1 比较元素的光标往前移一位
+                index--;
+            }
+            //遍历完毕 ，插入元素
+            //这里的index+1可以这样理解
+            //比如：[3,11,5]，
+            // 11 开始插入 此时的index = 0
+            // 3 比 11 大，11插入到index+1的位置上
+            array[index + 1] = cur;
+        }
+    }
+}
--- 代码/数据结构/src/Algorithm/SelectSort.java
+++ 代码/数据结构/src/Algorithm/SelectSort.java
@@ -17,16 +17,17 @@ public class SelectSort {
     }
 
     public static void selectSort(int[] array) {
+        //例如 一共有5个数据 只要确定4个数据的排序 最后一个不用再次排序
         for (int i = 0; i < array.length - 1; i++) {
-            //暂定光标指向的索引是最小值的索引
+            //暂定当前值的索引是最小值的索引
             int minIndex = i + 1;
-            //遍历光标后的所有元素,获得最小值的索引
+            //遍历当前值之后的所有元素,获得最小值的索引
             for (int j = i + 1; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     minIndex = j + 1;
                 }
             }
-            //交换光标指向的值和找到的最小值位置
+            //如果当前值小于array[minIndex]的值，则位置交换
             int temp;
             if (array[minIndex] < array[i]) {
                 temp = array[i];
